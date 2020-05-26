6335c142f1f5e6b2c18a35d568d30ee2960a14ee
Rohit Moni <rohitmdxb@gmail.com>
2020-05-26 00:34:16 -0400

    Added pop up UI that only shows up when on the HH builder page, that has a button that executes a test script

---
+++ popup.js
@@ -0,0 +1,14 @@
+let runButton = document.getElementById('runDecoExtractor');
+
+// chrome.storage.sync.get('color', function (data) {
+//     changeColor.style.backgroundColor = data.color;
+//     changeColor.setAttribute('value', data.color);
+// });
+
+runButton.onclick = function (element) {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        chrome.tabs.executeScript(
+            tabs[0].id,
+            { file: 'autoDecoExtractor.js' });
+    });
+};
