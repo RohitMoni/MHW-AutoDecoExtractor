92df90ccd5dfbbd25d72e33dda35bbcf34c7aa2b
Rohit Moni <rohitmdxb@gmail.com>
2020-05-26 21:21:22 -0400

    Button now overwrites deco data on Honey hunter correctly. Need to write the extraction stuff now

--- autoDecoExtractor.js
+++ autoDecoExtractor.js
@@ -1,1 +1,13 @@
-alert('test!');
+function extractDecoDataFromSavefile() {
+    
+}
+
+function writeDecoDataToDomInput(decoData) {
+    var decoDataInput = document.getElementById("decodatainput");
+    decoDataInput.value = decoData;
+
+    var rewriteDecoDataButton = document.getElementById("settingsbtnadddecos");
+    rewriteDecoDataButton.click();
+}
+
+writeDecoDataToDomInput("0, 0, 0, 0");
--- popup.js
+++ popup.js
@@ -1,10 +1,5 @@
 let runButton = document.getElementById('runDecoExtractor');
 
-// chrome.storage.sync.get('color', function (data) {
-//     changeColor.style.backgroundColor = data.color;
-//     changeColor.setAttribute('value', data.color);
-// });
-
 runButton.onclick = function (element) {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
         chrome.tabs.executeScript(
